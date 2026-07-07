5e7b2821075a6b97fee26cda1447a4c530ca116f
sunny raju <mallepogusunny123@gmail.com>
2026-07-07 14:42:46 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,12 +3,31 @@ const app = express()
 
 
 
-app.get("/user/:userId/:name",(req,res)=>{
-    console.log(req.params)
-    res.send({firstName:"Sunny",lastName:"Raju"})
-})
-
-
+app.use("/user",(req,res,next)=>{
+    console.log("handling the route user !!")
+    next()
+    // res.send("Response !!")
+},
+(req,res,next)=>{
+  console.log("handling the route user2 !!")
+//   res.send(" 2nd Response !!")
+next()
+},
+(req,res,next)=>{
+  console.log("handling the route user3 !!")
+//   res.send(" 3rd Response !!")
+next()
+},
+(req,res,next)=>{
+  console.log("handling the route user4 !!")
+//   res.send(" 4th Response !!")
+next()
+},
+(req,res,next)=>{
+  console.log("handling the route user5 !!")
+  res.send(" 5th Response !!")
+},
+)
 
 
 app.listen(3000,()=>{
